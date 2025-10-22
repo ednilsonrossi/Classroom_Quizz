50d6e9291e6d94f8b8834725d6664420fc78a3e2
Samuel Fernandes <samuelfernandesfilho2007@gmail.com>
2025-10-22 09:27:16 -0300

    Blueprint da rota Home para usuários logados | Inserção da página de Criação do Quiz.

---
+++ app/blueprints/home/routes.py
@@ -0,0 +1,24 @@
+from flask import render_template, redirect, url_for
+from forms import Pagina_Insercao_Codigo
+from flask_login import login_required
+from . import home
+
+@home.route('/')
+@login_required
+def homepage():
+    return render_template('home.html', title='Home')
+
+@home.route('/criar')
+@login_required
+def create_quiz_form():
+    return(render_template('create_quiz.html', title='Criar Quiz'))
+
+@home.route('/codigo_sala', methods=['GET', 'POST'])
+@login_required
+def codigo_sala():
+    form = Pagina_Insercao_Codigo()
+    if form.validate_on_submit():
+        return redirect(url_for('jogo.sala_espera'))
+
+    return render_template('codigo.html', title='Código da sala', form=form)
+
