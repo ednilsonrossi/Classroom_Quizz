d37cc3b589d220c187563ed7513b063ba2cfa9f8
Samuel Fernandes <samuelfernandesfilho2007@gmail.com>
2026-09-11 03:39:12 -0300

    Refatoração: centraliza extensões do Flask
    backend/
    └── app/
        └── utils/
            └── extensions.py
    
    Cria um módulo responsável por centralizar as instâncias das extensões utilizadas pela aplicação Flask.
    
    O arquivo extensions.py inicializa Bcrypt, utilizado para o gerenciamento seguro das senhas, e JWTManager, utilizado pelo sistema de autenticação baseado em JSON Web Tokens.
    
    As extensões são instanciadas de forma independente da aplicação Flask, sendo vinculadas depois à instância criada pelo padrão Application Factory através do método init_app().

--- backend/app/utils/extensions.py
+++ backend/app/utils/extensions.py
@@ -1,7 +1,5 @@
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
+from flask_jwt_extended import JWTManager
 
-#Criptografia do Banco de Dados
 bcrypt = Bcrypt()
-#Login de usuarios
-login_manager = LoginManager()
+jwt = JWTManager()
